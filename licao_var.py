c204fcf23eb1ca5380df22a9163c9a15edbb3675
Daniel <daniel.xdkk@gmail.com>
2024-06-13 23:40:46 -0300

    modificacoes no codigo e teste feito 13/06/24

--- licao_var.py
+++ licao_var.py
@@ -2,4 +2,4 @@ nome = input("Digite seu nome: ")
 altura = float(input("digite sua altura: "))
 idade = int(input("Digite sua idade: "))
 
-print(f"O usuário de nome: {nome}, tem {altura} metros de altura, e {idade} anos de idade.")
+print(f"O usuário de nome {nome}, tem {altura} metros de altura, e {idade} anos de idade.")
