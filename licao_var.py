ea3f3d48ba305acdd3091fe344de0bba56fb11aa
Daniel <daniel.xdkk@gmail.com>
2024-06-13 23:25:02 -0300

    codigo da licao de variaveis 13/06/24

---
+++ licao_var.py
@@ -0,0 +1,5 @@
+nome = input("Digite seu nome: ") 
+altura = float(input("digite sua altura: "))
+idade = int(input("Digite sua idade: "))
+
+print(f"O usuário de nome: {nome}, tem {altura} metros de altura, e {idade} anos de idade.")
