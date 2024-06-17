558146da94924f09f973fb2b24709ea531aae0b7
daniel <daniel.xdkk@gmail.com>
2024-06-17 20:46:11 -0300

    add atividade 2

---
+++ aula17-06/atividade_2.py
@@ -0,0 +1,15 @@
+#valor a ser calculado
+numero = int(input("Digite o numero a ser trabalhado: "))
+
+#contas matematicas 
+dobro = numero*2
+triplo = numero*3
+quadrado = numero**2
+raiz_quadrada = numero**(1/2)
+raiz_cubica = numero**(1/3)
+    
+#saidas de resposta - prints 
+print(f"O dobro do valor é: {dobro}")
+print(f"O triplo do valor é: {triplo}")
+print(f"A raiz quadrada do valor é: {raiz_quadrada}")
+print(f"A raiz cubica do valor é: {raiz_cubica}")
