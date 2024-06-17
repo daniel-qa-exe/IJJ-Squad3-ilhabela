a7e3aefbb197a3f6f72e9d0834d8197c9f9a4223
daniel <daniel.xdkk@gmail.com>
2024-06-17 19:59:58 -0300

    second commit

---
+++ aula17-06/aula17-06.py
@@ -0,0 +1,12 @@
+#lançamentos de notas 
+n1 = int(input("Digite a nota 1: "))
+n2 = int(input("Digite a nota 2: "))
+n3 = int(input("Digite a nota 3: "))
+n4 = int(input("Digite a nota 4: "))
+
+#calculos
+soma = n1 + n2 + n3 + n4
+media = soma / 4
+
+#saídas de texto
+print(f"Olá, Caique sua média é: {media} pontos")
