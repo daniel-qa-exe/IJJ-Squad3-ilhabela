25b216fec425d4d6b619b65658368a3d95e29ecf
daniel <daniel.xdkk@gmail.com>
2024-06-18 18:43:03 -0300

    atualizar o arquivbo collections

---
+++ aula17-06/collections.py
@@ -0,0 +1,24 @@
+#Lista
+frutas = [ 'Manga', 'Maracuja', 'Uva']
+#           0       1           2
+
+frutas.append("Melancia")
+print(frutas)
+
+print(frutas)
+print(frutas[1])
+
+#Tupla
+comidas = ('Hamburguer', 'Pizza', "Feijoada")
+print(comidas)
+
+# Set
+facilitadores = {"Vini", "Matheus", "Michelle"}
+print(facilitadores)
+print("Dicionario")
+dicionario = {
+    "nome": "Vini",
+    "idade": 25,
+}
+
+print(dicionario["nome"])
