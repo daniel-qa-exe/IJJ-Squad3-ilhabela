93c3590019ad62aa5a7e46533b17885982458d88
daniel <daniel.xdkk@gmail.com>
2024-06-19 19:45:43 -0300

    add branches and add calculadora folder

---
+++ caluladora/calculadora.py
@@ -0,0 +1,13 @@
+"""
+logica funcional 
+
+input operador
+input numero 1 
+input numero 2
+
+match case pra fazer a comparação desse input 
+
+cases com a operação
+    o calculo com o retorno
+
+"""
