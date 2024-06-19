e7a88f49f7a2207e8188433104c6dfdc9a37bc82
daniel <daniel.xdkk@gmail.com>
2024-06-18 21:13:29 -0300

    add arquivo juntoFit

---
+++ aula18-06/juntoFit.py
@@ -0,0 +1,8 @@
+
+frequencia = int(input("frequencia do aluno: " ))
+dias_ok = 21
+print("VOCÊ ESTÁ PARTICIPANDO DA NOSSA PROMO TREINA JUNTO")
+if frequencia == 21:
+    print("UHUU. AGORA VOCÊ PODE PRESENTEAR UM AMIGO OU AMIGA PARA TREINAR COM VOCÊ")
+elif frequencia > 1 and frequencia < 21:
+    print("QUE BOM VER VOCÊ DE VOLTA. A PARTIR DE AGORA INICIAMOS MAIS UMA CONTAGEM DE 21 DIAS PARA A PROMO TREINA JUNTO.")
