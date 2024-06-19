729a99e324ac6c04f06d9509da6124445a0e52b7
daniel <daniel.xdkk@gmail.com>
2024-06-19 20:07:02 -0300

    create branches and folder calculadora

--- caluladora/calculadora.py
+++ caluladora/calculadora.py
@@ -11,3 +11,21 @@ cases com a operação
     o calculo com o retorno
 
 """
+print("Calculadora simples, calcule soma ,subtração, multiplicação ou divisão")
+operador = input("Escolha uma operação (+, -, *, /): ")
+numero1 = int(input("Digite o primeiro numero: "))
+numero2 = int(input("Digite o segundo numero: "))
+
+match operador:
+    case  "+":
+        soma = numero1 + numero2
+        print(f"O resultado do seu calculo é {soma}")
+    case "-":
+        subt = numero1 - numero2
+        print(f"O resultado do seu calculo é {subt}")
+    case "*":
+        mult = numero1 * numero2
+        print(f"O resultado do seu calculo é {mult}")
+    case "/":
+        div = numero1 / numero2
+        print(f"O resultado do seu calculo é {div}")
